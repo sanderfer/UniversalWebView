03c95ede28698b77a65f5bdee40c026dc44c6671
fanzehua <fzh_624@qq.com>
2020-01-03 15:36:53 +0800

    fix

--- app/src/main/java/com/fan/web/MainActivity.kt
+++ app/src/main/java/com/fan/web/MainActivity.kt
@@ -8,5 +8,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        mainWeb.load("http://118.126.111.114/image/test.pdf")
     }
 }
